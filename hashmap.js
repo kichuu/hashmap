db1c0b94653577a87fedc7e004d8481193016480
krishnadev <krishnadevkichus@gmail.com>
2024-10-07 22:02:15 +0530

    added resize bucket function

--- hashmap.js
+++ hashmap.js
@@ -1,11 +1,34 @@
 class HashMap {
   constructor(initalCapacity = 16) {
     this.capacity = initalCapacity;
-    this.buckets = new Array(initalCapacity);
+    this.buckets = new Array(this.capacity);
     this.count = 0
+    this.loadfactor = 0.75
   }
 
+  resizeBucket() {
+    const oldBuckets = this.buckets;
+    this.capacity *= 2; 
+    this.buckets = new Array(this.capacity); 
+    this.count = 0; 
+
   
+    for (let i = 0; i < oldBuckets.length; i++) {
+        const bucket = oldBuckets[i];
+        if (bucket) {
+            for (let j = 0; j < bucket.length; j++) {
+                const [key, value] = bucket[j];
+                const index = this.hash(key); 
+                if (this.buckets[index] === undefined) {
+                    this.buckets[index] = [];
+                }
+                this.buckets[index].push([key, value]); 
+                this.count++; 
+            }
+        }
+    }
+}
+
   hash(key) {
     let hashCode = 0;
 
@@ -18,6 +41,9 @@ class HashMap {
   }
 
   set(key, value) {
+    if(this.count >= (this.loadfactor*this.capacity)){
+      this.resizeBucket()
+    }
     let index = this.hash(key);
     if (this.buckets[index] == undefined) {
       this.buckets[index] = [];
@@ -88,7 +114,7 @@ class HashMap {
     let keysArr =[]
     for(let i=0;i<this.buckets.length;i++){
       if(this.buckets[i] != null || this.buckets[i] != undefined){
-        for(let j=0;j<this.buckets[i].length;i++){
+        for(let j=0;j<this.buckets[i].length;j++){
           keysArr.push(this.buckets[i][j][0])
         }
       }
@@ -100,8 +126,8 @@ class HashMap {
     let valArr =[]
     for(let i=0;i<this.buckets.length;i++){
       if(this.buckets[i] != null || this.buckets[i] != undefined){
-        for(let j=0;j<this.buckets[i].length;i++){
-          keysArr.push(this.buckets[i][j][1])
+        for(let j=0;j<this.buckets[i].length;j++){
+          valArr.push(this.buckets[i][j][1])
         }
       }
     }
@@ -112,10 +138,13 @@ class HashMap {
     let entriesArr = []
     for(let i=0;i<this.buckets.length;i++){
       if(this.buckets[i] != null || this.buckets[i] != undefined){
-        for(let j=0;j<this.buckets[i].length;i++){
+        for(let j=0;j<this.buckets[i].length;j++){
           entriesArr.push(this.buckets[i][j])
         }
       }
     }
+    return entriesArr
   }
 }
+
+export default HashMap
